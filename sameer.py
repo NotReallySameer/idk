44dd250becffb029174b41f7c3d1566ba3ca44c5
Sameer Khan <125715243+NotReallySameer@users.noreply.github.com>
2023-02-18 10:41:44 +0530

    Update sameer.py

--- sameer.py
+++ sameer.py
@@ -1,1 +1,19 @@
+from pyrogram import Client, filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+
+
+SAMEER= Client(
+    name="FirstProject",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+print("YOUR BOT STARTED SUCCESSFULLY")
+
+SAMEER.run()
