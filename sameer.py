157643cfc0e98e30125d4c91a8c5c567ae824953
Sameer Khan <125715243+NotReallySameer@users.noreply.github.com>
2023-02-18 13:33:11 +0530

    Update sameer.py

--- sameer.py
+++ sameer.py
@@ -15,14 +15,14 @@ SAMEER= Client(
 )
 
 ###thia is a start function
-@SAMEER.on_messege(filter.command("start"))
+@SAMEER.on_messege(filters.command("start"))
 async def start_cmd(client, message):
-    print("hello i am sammy first project don't start me again")
+    await message.reply_text("hello i am sammy first project don't start me again")
 
 ###this is a help function lawde log
-@SAMEER.on_messege(filter.command("help"))
+@SAMEER.on_messege(filters.command("help"))
 async def help_cmd(client, message):
-    print("lawda dekhne aaya hai kya biro isme its a test bot only jhatu")
+    await message.reply_text("lawda dekhne aaya hai kya biro isme its a test bot only jhatu")
 
 print("YOUR BOT STARTED SUCCESSFULLY")
 
